ece17ab18ca85cd579087bb4a27b621a408d897e
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-01-14 11:30:44 +0100

    Updated CMakeList.txt

---
+++ lal/utils/std_out.hpp
@@ -0,0 +1,50 @@
+/*********************************************************************
+ *
+ *  Linear Arrangement Library - A library that implements a collection
+ *  algorithms for linear arrangments of graphs.
+ *
+ *  Copyright (C) 2019
+ *
+ *  This file is part of Linear Arrangement Library.
+ *
+ *  Linear Arrangement Library is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Affero General Public License
+ *  as published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Linear Arrangement Library is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Contact:
+ *
+ *      Lluís Alemany Puig (lalemany@cs.upc.edu)
+ *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *          CQL (Complexity and Quantitative Linguistics Lab)
+ *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *          Research Gate: https://www.researchgate.net/profile/Lluis_Alemany-Puig
+ *
+ *      Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
+ *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *          CQL (Complexity and Quantitative Linguistics Lab)
+ *          Office S124, Omega building
+ *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *          Webpage: https://www.cs.upc.edu/~rferrericancho/
+ *          Research Gate: https://www.researchgate.net/profile/Ramon_Ferrer-i-Cancho
+ *
+ *      Juan Luis Esteban (esteban@cs.upc.edu)
+ *          LOGPROG: Logics and Programming Research Group
+ *          Office 110, Omega building
+ *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *          Webpage: https://www.cs.upc.edu/~esteban/
+ *          Research Gate: https://www.researchgate.net/profile/Juan_Esteban13
+ *
+ ********************************************************************/
+#ifndef STD_OUT_HPP
+#define STD_OUT_HPP
+
+#endif // STD_OUT_HPP
