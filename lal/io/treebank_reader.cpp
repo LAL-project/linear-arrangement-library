d116bf6cbf437636d1442c4a0b71efaa6930ea88
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-01-18 18:24:51 +0100

    Updated year in License header

--- lal/io/treebank_reader.cpp
+++ lal/io/treebank_reader.cpp
@@ -3,7 +3,7 @@
  *  Linear Arrangement Library - A library that implements a collection
  *  algorithms for linear arrangments of graphs.
  *
- *  Copyright (C) 2019
+ *  Copyright (C) 2019-2020
  *
  *  This file is part of Linear Arrangement Library.
  *
