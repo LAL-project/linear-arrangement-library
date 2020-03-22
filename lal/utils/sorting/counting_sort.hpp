3fb39e78b52ad33b2e4e89c0110c5622d9f3e06c
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-03-22 11:35:24 +0100

    Use C arrays instead of vector<>

--- lal/utils/sorting/counting_sort.hpp
+++ lal/utils/sorting/counting_sort.hpp
@@ -40,6 +40,9 @@
 
 #pragma once
 
+// C includes
+#include <string.h>
+
 // C++ includes
 #include <functional>
 #include <vector>
@@ -58,6 +61,8 @@ namespace utils {
  *
  * @param begin Iterator at the beginning of the range.
  * @param end Iterator at the end of the range.
+ * @param M Integer value that has to be, at least, one unit larger than the
+ * largest key obtained with function @e key.
  * @param key Function that returns an integer value used to compare the elements.
  * @post The elements in the range [begin,end) are sorted increasingly.
  */
@@ -66,35 +71,43 @@ template<
 	typename T = typename std::iterator_traits<It>::value_type
 >
 void counting_sort(
-	It begin, It end, const std::function<size_t (const T&)>& key
+	It begin, It end, const size_t M, const std::function<size_t (const T&)>& key
 )
 {
-	// maximum & minimum elements within vector
-	const auto M_it = std::max_element(begin, end);
+	const size_t S = std::distance(begin, end);
 
-	const size_t Mk = key(*M_it);
-	std::vector<size_t> count(Mk + 1, 0);
+	// allocate memory
+	T *output = static_cast<T *>(malloc(S*sizeof(T)));
+	size_t *count = static_cast<size_t *>(malloc(M*sizeof(size_t)));
 
+	// initialise memory (when needed)
+	memset(count, 0, M*sizeof(size_t));
+
+	// calculate frequency of each element
 	for (auto it = begin; it != end; ++it) {
-		count[key(*it)] += 1;
+		const size_t elem_key = key(*it);
+		count[elem_key] += 1;
 	}
 
 	size_t total = 0;
-	for (size_t k = 0; k <= Mk; ++k) {
+	for (size_t k = 0; k < M; ++k) {
 		std::tie(count[k], total)
 			= std::make_pair(total, count[k] + total);
 	}
-
-	const size_t S = std::distance(begin, end);
-	std::vector<T> output(S);
-	for (auto it = begin; it != end; ++it) {
-		output[count[key(*it)]] = *it;
-		count[key(*it)] += 1;
+	auto it = begin;
+	for (; it != end; ++it) {
+		const size_t elem_key = key(*it);
+		output[count[elem_key]] = *it;
+		count[elem_key] += 1;
 	}
 
-	auto it1 = begin;
-	auto it2 = output.begin();
-	for (size_t k = 0; k < S; ++k, ++it1, ++it2) { *it1 = *it2; }
+	// calculate output
+	it = begin;
+	for (size_t k = 0; k < S; ++k, ++it) { *it = output[k]; }
+
+	// free memory
+	free(output);
+	free(count);
 }
 
 } // -- namespace utils
