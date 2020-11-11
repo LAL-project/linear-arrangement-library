e1f178297fb09793fc9755462b2cac51cbaafb2d
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-11-11 23:20:10 +0100

    Reduced the code's complexity
    So far: two functions (ugh!), now there's only one.
    Some minor changes like renaming variables and added some more comments.

--- lal/properties/variance_C_gen_graphs.cpp
+++ lal/properties/variance_C_gen_graphs.cpp
@@ -52,9 +52,6 @@ using namespace std;
 
 typedef uint64_t bigint;
 
-// lal includes
-#include <lal/iterators/E_iterator.hpp>
-
 #define sorted_edge(u,v) (u < v ? edge(u,v) : edge(v,u))
 #define map_has_key(MAP, K, it) ((it = MAP.find(K)) != MAP.end())
 
@@ -72,8 +69,8 @@ typedef uint64_t bigint;
 			++__i; ++__j;							\
 		}											\
 		else {										\
-			auto __Ni_i = Ni[__i];					\
-			auto __Nj_j = Nj[__j];					\
+			const auto __Ni_i = Ni[__i];			\
+			const auto __Nj_j = Nj[__j];			\
 			__i += (__Ni_i < __Nj_j);				\
 			__j += (__Ni_i > __Nj_j);				\
 		}											\
@@ -82,13 +79,10 @@ typedef uint64_t bigint;
 
 struct useful_info_pairs {
 	// number of common neighbours
-	uint64_t common;
+	uint64_t common = 0;
 	// sum of the degrees of the common nodes
-	uint64_t sum_deg_common;
+	uint64_t sum_deg_common = 0;
 
-	useful_info_pairs() {
-		common = sum_deg_common = 0;
-	}
 	useful_info_pairs(uint64_t _common, uint64_t _sdc)
 		: common(_common), sum_deg_common(_sdc)
 	{
@@ -108,145 +102,14 @@ namespace properties {
 // Variance of C (using formula)
 // GENERAL GRAPHS
 
-inline void compute_data_gen_graphs_no_reuse
-(
-	const undirected_graph& g, const bigint& n, const bigint& m,
-	bigint& Qs, bigint& Kg,
-	bigint& n_paths_4, bigint& n_cycles_4, bigint& paw,
-	bigint& n_paths_5, bigint& pair_C3_L2,
-	bigint& Phi_1, bigint& Phi_2,
-	bigint& Lambda_1, bigint& Lambda_2
-)
-{
-	// ------------------------------------------------
-	// local variables (some store precomputed data)
-
-	// allocate memory for all arrays
-	bigint *xi = new bigint[n]{0};
-	// sum of degrees squared
-	bigint nk2 = 0;
-	// sum of degrees cubed
-	bigint nk3 = 0;
-	// sum_{st in E} k_s*k_t
-	bigint psi = 0;
-
-	// ------------------------------------------------
-	// precompute data
-
-	for (node s = 0; s < n; ++s) {
-		const bigint ks = g.degree(s);
-		nk2 += ks*ks;
-		nk3 += ks*ks*ks;
-
-		xi[s] = 0;
-		for (node t : g.get_neighbours(s)) {
-			const bigint kt = g.degree(t);
-			psi += ks*kt;
-			xi[s] += kt;
-		}
-	}
-	psi /= 2;
-
-	// ------------------------------------------------
-	// compute the variance
-
-	Qs = (m*(m + 1) - nk2)/2;
-	Kg = (m + 1)*nk2 - nk3 - 2*psi;
-	Phi_1 = (m + 1)*psi;
-	Phi_2 = 0;
-
-	bigint mu1 = 0;
-	bigint mu2 = 0;
-
-	E_iterator it(g);
-	while (it.has_next()) {
-		it.next();
-		const edge e = it.get_edge();
-		// first node
-		const node s = e.first;
-		const bigint ks = g.degree(s);
-		const neighbourhood& Ns = g.get_neighbours(s);
-		// second node
-		const node t = e.second;
-		const bigint kt = g.degree(t);
-		const neighbourhood& Nt = g.get_neighbours(t);
-
-		// for each neighbour of 's' different from 't'
-		for (node u : Ns) {
-			if (u == t) { continue; }
-
-			const neighbourhood& Nu = g.get_neighbours(u);
-			const bigint ku = g.degree(u);
-
-			bigint common_ut = 0;
-			iterate(Nu, Nt,
-				++common_ut;
-			)
-
-			const bool ut_neighs = std::binary_search(Nt.begin(), Nt.end(), u);
-			n_paths_5 += (kt - 1 - ut_neighs)*(ku - 1 - ut_neighs) + 1;
-			n_paths_5 -= common_ut;
-		}
-		// for each neighbour of 't' different from 's'
-		for (node u : Nt) {
-			if (u == s) { continue; }
-
-			const neighbourhood& Nu = g.get_neighbours(u);
-			const bigint ku = g.degree(u);
-
-			bigint common_us = 0;
-			iterate(Nu, Ns,
-				++common_us;
-			)
-
-			const bool us_neighs = std::binary_search(Ns.begin(), Ns.end(), u);
-			n_paths_5 += (ks - 1 - us_neighs)*(ku - 1 - us_neighs) + 1;
-			n_paths_5 -= common_us;
-
-			n_cycles_4 += common_us - 1;
-		}
-
-		bigint deg_sum_st = 0;
-		bigint common_st = 0;
-		iterate(Ns, Nt,
-			++common_st;
-			deg_sum_st += g.degree(Ns[__i]);
-		)
-
-		paw += deg_sum_st - 2*common_st;
-		pair_C3_L2 += (m - ks - kt + 3)*common_st - deg_sum_st;
-
-		Phi_1 -= ks*kt*(ks + kt);
-		Phi_2 += (ks + kt)*(nk2 - (ks*(ks - 1) + kt*(kt - 1)) - xi[s] - xi[t]);
-
-		mu1 += xi[t] + xi[s];
-		mu2 += common_st;
-
-		Lambda_1 += (kt - 1)*(xi[s] - kt) + (ks - 1)*(xi[t] - ks);
-		Lambda_1 -= 2*deg_sum_st;
-
-		Lambda_2 += (ks + kt)*( (ks - 1)*(kt - 1) - common_st );
-	}
-
-	Lambda_2 += Lambda_1;
-
-	Phi_2 /= 2;
-	n_paths_4 = m - nk2 + mu1/2 - mu2;
-	n_cycles_4 /= 4;
-	n_paths_5 /= 2;
-	pair_C3_L2 /= 3;
-
-	// deallocate memory
-	delete[] xi;
-}
-
 /* Warning:
- * 'pair_C3_L2' is, by 3 times, larger than the actual amount of
+ * 'pair_C3_L2' is 3 times larger than the actual amount of
  * pairs of disjoint cycle of 3 nodes and edge. This is done
  * so that we don't have to change the rational numbers used to
  * compute the variance.
  */
-inline void compute_data_gen_graphs_reuse
+template<bool reuse>
+void compute_data_gen_graphs
 (
 	const undirected_graph& g, const bigint& n, const bigint& m,
 	bigint& Qs, bigint& Kg,
@@ -284,6 +147,9 @@ inline void compute_data_gen_graphs_reuse
 			xi[s] += kt;
 		}
 	}
+	// note: at this point 'psi' is equal to
+	// 2*sum_{st in E} k_s*k_t
+	assert(psi%2 == 0);
 	psi /= 2;
 
 	// hash table to reuse computations
@@ -304,99 +170,116 @@ inline void compute_data_gen_graphs_reuse
 	while (it.has_next()) {
 		it.next();
 		const edge e = it.get_edge();
-		// first node
 		const node s = e.first;
 		const bigint ks = g.degree(s);
 		const neighbourhood& Ns = g.get_neighbours(s);
-		// second node
 		const node t = e.second;
 		const bigint kt = g.degree(t);
 		const neighbourhood& Nt = g.get_neighbours(t);
 
+		// for each neighbour of 's' different from 't'
 		for (node u : Ns) {
 			if (u == t) { continue; }
 
+			const neighbourhood& Nu = g.get_neighbours(u);
+			const bigint ku = g.degree(u);
+
 			bigint common_ut = 0;
-			if (CIT it_ut; map_has_key(saving_comps, sorted_edge(u,t), it_ut)) {
-				common_ut = it_ut->second.common;
+			if constexpr (reuse) {
+				if (CIT it_ut; map_has_key(saving_comps, sorted_edge(u,t), it_ut)) {
+					common_ut = it_ut->second.common;
+				}
+				else {
+					// compute values and store them
+					bigint deg_sum = 0;
+					iterate(Nt, Nu,
+						++common_ut;
+						deg_sum += g.degree(Nu[__j]);
+					)
+					saving_comps.insert(
+						make_pair(
+							sorted_edge(u,t),
+							useful_info_pairs(common_ut, deg_sum)
+						));
+				}
 			}
 			else {
-				const neighbourhood& Nu = g.get_neighbours(u);
-				bigint deg_sum = 0;
-				iterate(Nt, Nu,
+				iterate(Nu, Nt,
 					++common_ut;
-					deg_sum += g.degree(Nu[__j]);
 				)
-				saving_comps.insert(
-					make_pair(
-						sorted_edge(u,t),
-						useful_info_pairs(common_ut, deg_sum)
-					));
 			}
 
-			const bool ut_neighs = std::binary_search(Nt.begin(), Nt.end(), u);
-			const bigint ku = g.degree(u);
-			n_paths_5 += (kt - 1 - ut_neighs)*(ku - 1 - ut_neighs) + 1 - common_ut;
+			const bool ut_is_edge = g.has_edge(u,t);
+			n_paths_5 += (kt - 1 - ut_is_edge)*(ku - 1 - ut_is_edge) + 1 - common_ut;
 		}
-
+		// for each neighbour of 't' different from 's'
 		for (node u : Nt) {
 			if (u == s) { continue; }
 
-			// precompute data
+			const neighbourhood& Nu = g.get_neighbours(u);
+			const bigint ku = g.degree(u);
+
 			bigint common_us = 0;
-			if (CIT it_su; map_has_key(saving_comps, sorted_edge(u,s), it_su)) {
-				common_us = it_su->second.common;
+			if constexpr (reuse) {
+				if (CIT it_su; map_has_key(saving_comps, sorted_edge(u,s), it_su)) {
+					common_us = it_su->second.common;
+				}
+				else {
+					// compute values and store them
+					bigint deg_sum = 0;
+					iterate(Ns, Nu,
+						++common_us;
+						deg_sum += g.degree(Nu[__j]);
+					)
+					saving_comps.insert(
+						make_pair(
+							sorted_edge(u,s),
+							useful_info_pairs(common_us, deg_sum)
+						));
+				}
 			}
 			else {
-				// compute values and store them
-				const neighbourhood& Nu = g.get_neighbours(u);
-				bigint deg_sum = 0;
-				iterate(Ns, Nu,
+				iterate(Nu, Ns,
 					++common_us;
-					deg_sum += g.degree(Nu[__j]);
 				)
-				saving_comps.insert(
-					make_pair(
-						sorted_edge(u,s),
-						useful_info_pairs(common_us, deg_sum)
-					));
 			}
 
-			const bool us_neighs = std::binary_search(Ns.begin(), Ns.end(), u);
-			const bigint ku = g.degree(u);
-			n_paths_5 += (ks - 1 - us_neighs)*(ku - 1 - us_neighs) + 1 - common_us;
-
+			const bool is_us_edge = g.has_edge(u,s);
+			n_paths_5 += (ks - 1 - is_us_edge)*(ku - 1 - is_us_edge) + 1 - common_us;
 			n_cycles_4 += common_us;
 		}
 
-		// number of common nodes
+		n_cycles_4 -= kt - 1;
+
 		bigint common_st = 0;
-		// sum of the degrees of the common nodes
 		bigint deg_sum_st = 0;
-		if (CIT it_st; map_has_key(saving_comps, sorted_edge(s,t), it_st)) {
-			// if the neighbours were not searched then the variables
-			// 'sum_deg_common_st' and 'common_st' are equal to '0',
-			// so we must initialise them now
-			deg_sum_st = it_st->second.sum_deg_common;
-			common_st = it_st->second.common;
+		if constexpr (reuse) {
+			if (CIT it_st; map_has_key(saving_comps, sorted_edge(s,t), it_st)) {
+				// if the neighbours were not searched then the variables
+				// 'sum_deg_common_st' and 'common_st' are equal to '0',
+				// so we must initialise them now
+				deg_sum_st = it_st->second.sum_deg_common;
+				common_st = it_st->second.common;
+			}
+			else {
+				// compute values and store them
+				iterate(Ns, Nt,
+					++common_st;
+					deg_sum_st += g.degree(Nt[__j]);
+				)
+				saving_comps.insert(
+					make_pair(
+						sorted_edge(s,t),
+						useful_info_pairs(common_st, deg_sum_st)
+					));
+			}
 		}
 		else {
-			// compute values and store them
-			// common neighbours of 's' and 't'
-
 			iterate(Ns, Nt,
 				++common_st;
-				deg_sum_st += g.degree(Nt[__j]);
+				deg_sum_st += g.degree(Ns[__i]);
 			)
-
-			saving_comps.insert(
-				make_pair(
-					sorted_edge(s,t),
-					useful_info_pairs(common_st, deg_sum_st)
-				));
 		}
-
-		n_cycles_4 -= kt - 1;
 
 		paw += deg_sum_st - 2*common_st;
 		pair_C3_L2 += common_st*(m - ks - kt + 3) - deg_sum_st;
@@ -412,6 +295,12 @@ inline void compute_data_gen_graphs_reuse
 
 		Lambda_2 += (ks + kt)*( (ks - 1)*(kt - 1) - common_st );
 	}
+
+	assert(Phi_2%2 == 0);
+	assert(mu1%2 == 0);
+	assert(n_cycles_4%4 == 0);
+	assert(n_paths_5%2 == 0);
+	assert(pair_C3_L2%3 == 0);
 
 	Lambda_2 += Lambda_1;
 	Phi_2 /= 2;
@@ -463,7 +352,7 @@ rational variance_C_rational(const undirected_graph& g, bool reuse) {
 	bigint Lambda_2 = 0;
 
 	if (reuse) {
-		compute_data_gen_graphs_reuse
+		compute_data_gen_graphs<true>
 		(
 			g, n, m,
 			Qs, Kg,
@@ -474,7 +363,7 @@ rational variance_C_rational(const undirected_graph& g, bool reuse) {
 		);
 	}
 	else {
-		compute_data_gen_graphs_no_reuse
+		compute_data_gen_graphs<false>
 		(
 			g, n, m,
 			Qs, Kg,
