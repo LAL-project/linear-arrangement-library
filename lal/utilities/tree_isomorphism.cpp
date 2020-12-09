eb8470b49053e6d5be914f2ac9bffef130c21337
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2020-12-09 19:48:14 +0100

    Tried a more complicated algorithm
    ... to speed up calculations when trees are not isomorphic,
    but it is slower for small trees (n ~ 10 vertices).

--- lal/utilities/tree_isomorphism.cpp
+++ lal/utilities/tree_isomorphism.cpp
@@ -129,6 +129,139 @@ void assign_name(const rooted_tree& t, node v, string& name) {
 
 // -----------------------------------------------------------------------------
 
+/*
+class test_isomorphism {
+public:
+	test_isomorphism(const rooted_tree& __t1, const rooted_tree& __t2)
+	: t1(__t1), t2(__t2)
+	{ }
+
+	bool are_trees_isomorphic() {
+		make_names_per_level<false>(
+			t1,
+			nodes_per_level_t1, name_per_level_t1, name_per_node_t1
+		);
+
+		return make_names_per_level<true>
+		(
+			t2,
+			nodes_per_level_t2, name_per_level_t2, name_per_node_t2
+		);
+	}
+
+	void retrieve_nodes_per_level(
+		const rooted_tree& t, vector<vector<node>>& nodes_per_level
+	)
+	{
+		const uint32_t n = t.n_nodes();
+		nodes_per_level = vector<vector<node>>(n);
+
+		nodes_per_level[0].push_back(t.get_root());
+
+		internal::BFS<rooted_tree> bfs(t);
+		bfs.set_use_rev_edges(false);
+
+		uint32_t max_level = 0;
+		vector<uint32_t> level(n, 0);
+		bfs.set_process_neighbour(
+		[&](const auto&, node u, node v, bool) -> void {
+			level[v] = level[u] + 1;
+			nodes_per_level[level[v]].push_back(v);
+
+			max_level = std::max(max_level, level[v]);
+		}
+		);
+		bfs.start_at(t.get_root());
+
+		nodes_per_level.resize(max_level + 1);
+#if defined DEBUG
+		assert(nodes_per_level.size() == max_level + 1);
+#endif
+	}
+
+	template<bool compare>
+	using Type = typename std::conditional<compare, bool, void>::type;
+	template<bool compare> Type<compare>
+	make_names_per_level
+	(
+		const rooted_tree& t,
+		vector<vector<node>>& nodes_per_level,
+		vector<string>& name_per_level,
+		vector<string>& name_per_node
+	)
+	{
+		const uint32_t n = t.n_nodes();
+
+		retrieve_nodes_per_level(t, nodes_per_level);
+		name_per_level = vector<string>(nodes_per_level.size());
+		name_per_node = vector<string>(n);
+
+		for (int l = to_int(nodes_per_level.size()) - 1; l >= 0; --l) {
+			const auto& nodes_level_l = nodes_per_level[l];
+			vector<string> pieces_of_level(nodes_level_l.size());
+
+			size_t i = 0;
+			for (node u : nodes_level_l) {
+				// store the neighbours names in a vector for later sorting
+				vector<string> pieces_of_node(t.out_degree(u));
+				size_t j = 0;
+				for (node v : t.get_out_neighbours(u)) {
+					pieces_of_node[j++] = name_per_node[v];
+				}
+				sort(pieces_of_node.begin(), pieces_of_node.end());
+
+				// build the name for this node
+				name_per_node[u] = "1";
+				for (const string& s : pieces_of_node) { name_per_node[u] += s; }
+				name_per_node[u] += "0";
+
+				// collect another piece of this level
+				pieces_of_level[i] = name_per_node[u];
+				++i;
+			}
+			// sort the level's pieces
+			sort(pieces_of_level.begin(), pieces_of_level.end());
+			// build the level's name
+			for (const auto& s : pieces_of_level) { name_per_level[l] += s; }
+
+			if constexpr (compare) {
+			if (name_per_level[l] != name_per_level_t1[l]) { return false; }
+			}
+		}
+
+		if constexpr (compare) {
+			return true;
+		}
+	}
+
+private:
+	const rooted_tree& t1, t2;
+
+	vector<vector<node>> nodes_per_level_t1;
+	vector<string> name_per_level_t1;
+	vector<string> name_per_node_t1;
+
+	vector<vector<node>> nodes_per_level_t2;
+	vector<string> name_per_level_t2;
+	vector<string> name_per_node_t2;
+};
+
+// -----------------------------------------------------------------------------
+
+bool are_trees_isomorphic(const rooted_tree& t1, const rooted_tree& t2) {
+	if (t1.is_orientation_valid() != t2.is_orientation_valid()) {
+		return false;
+	}
+
+	const int discard = fast_non_iso(t1,t2);
+	if (discard == 0) { return true; }
+	if (discard == 1) { return false; }
+
+	test_isomorphism ti(t1, t2);
+	return ti.are_trees_isomorphic();
+}
+*/
+
 bool are_trees_isomorphic(const rooted_tree& t1, const rooted_tree& t2) {
 	if (t1.is_orientation_valid() != t2.is_orientation_valid()) {
 		return false;
