93011689e3f60efaebd88ca36b14350a96fbb0e0
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-02-02 16:01:24 +0100

    Extended counting sort to reuse memory

--- lal/internal/sorting/counting_sort.hpp
+++ lal/internal/sorting/counting_sort.hpp
@@ -42,10 +42,104 @@
 #pragma once
 
 // C++ includes
-#include <functional>
+#include <algorithm>
+#include <type_traits>
+
+// lal includes
+#include <lal/internal/data_array.hpp>
 
 namespace lal {
 namespace internal {
+
+template<typename T>
+struct memory_counting_sort {
+	data_array<std::size_t> count;
+	data_array<T> output;
+
+	memory_counting_sort(
+		const std::size_t largest_key_plus_1,
+		const std::size_t max_size_container
+	) :
+	count(largest_key_plus_1, 0), output(max_size_container)
+	{ }
+
+	~memory_counting_sort() { }
+
+	void reset_count() { count.fill(0); }
+};
+
+/*
+ * @brief Counting sort algorithm with reusable memory.
+ *
+ * This algorithm is interesting for sorting containers with non-unique values.
+ * For details on the algorithm, see https://en.wikipedia.org/wiki/Counting_sort
+ *
+ * Template parameters:
+ * @param It Iterator type
+ * @param T Iterated type
+ * @param increasing: when true, the result is sorted increasingly. When false,
+ * the result is sorted decreasingly.
+ *
+ * Function paremeters:
+ * @param begin Iterator at the beginning of the range.
+ * @param end Iterator at the end of the range.
+ * @param largest_key_plus_1 Integer value equal to 1 + the largest key that can
+ * be obtained with function @e key.
+ * @param key Function that returns a single integer value used to compare the
+ * elements.
+ * @post The elements in the range [begin,end) are sorted increasingly.
+ */
+template<
+	typename It,
+	typename T = typename std::iterator_traits<It>::value_type,
+	bool increasing
+>
+void counting_sort(
+	It begin, It end,
+	const std::size_t largest_key_plus_1,
+	const std::function<std::size_t (const T&)>& key,
+	memory_counting_sort<T>& mem
+)
+{
+	// nothing to do if there are no elements to sort
+	if (begin == end) { return; }
+
+	// calculate frequency of each element
+	for (auto it = begin; it != end; ) {
+		const std::size_t elem_key = key(*(it++));
+		mem.count[elem_key] += 1;
+	}
+
+	std::size_t total = 0;
+	for (std::size_t k = 0; k < largest_key_plus_1; ++k) {
+		std::tie(mem.count[k], total)
+			= std::make_pair(total, mem.count[k] + total);
+	}
+
+	std::size_t size_container = 0;
+	for (auto it = begin; it != end; ) {
+		++size_container;
+
+		const std::size_t elem_key = key(*it);
+		mem.output[mem.count[elem_key]] = std::move(*(it++));
+		mem.count[elem_key] += 1;
+	}
+
+	// calculate output
+	if constexpr (increasing) {
+		auto it = begin;
+		for (std::size_t k = 0; k < size_container;) {
+			*(it++) = std::move(mem.output[k++]);
+		}
+	}
+	else {
+		auto it = begin;
+		for (std::size_t k = size_container - 1; k > 0;) {
+			*(it++) = std::move(mem.output[k--]);
+		}
+		*it = std::move(mem.output[0]);
+	}
+}
 
 /*
  * @brief Counting sort algorithm.
@@ -62,10 +156,11 @@ namespace internal {
  * Function paremeters:
  * @param begin Iterator at the beginning of the range.
  * @param end Iterator at the end of the range.
- * @param _M Integer value equal to the largest key that can be obtained with
+ * @param M Integer value equal to the largest key that can be obtained with
  * function @e key.
- * @param S The size of the container to be sorted. This is equal to
- * std::distance(begin,end).
+ * @param upper_bound_size An upper bound of he size of the container to be
+ * sorted. The lowest value is std::distance(begin,end), the actual size of the
+ * container.
  * @param key Function that returns a single integer value used to compare the
  * elements.
  * @post The elements in the range [begin,end) are sorted increasingly.
@@ -76,53 +171,21 @@ template<
 	bool increasing
 >
 void counting_sort(
-	It begin, It end, std::size_t M, const std::size_t S,
+	It begin, It end,
+	const std::size_t largest_key,
+	const std::size_t upper_bound_size,
 	const std::function<std::size_t (const T&)>& key
 )
 {
 	// nothing to do if there are no elements to sort
 	if (begin == end) { return; }
 
-	++M;
+	memory_counting_sort<T> mem(largest_key+1, upper_bound_size);
 
-	// allocate memory
-	T *output = new T[S];
-	std::size_t *count = new std::size_t[M]{0};
+	counting_sort<It, T, increasing>
+	(begin, end, largest_key+1, key, mem);
 
-	// calculate frequency of each element
-	for (auto it = begin; it != end; ) {
-		const std::size_t elem_key = key(*(it++));
-		count[elem_key] += 1;
-	}
-
-	std::size_t total = 0;
-	for (std::size_t k = 0; k < M; ++k) {
-		std::tie(count[k], total)
-			= std::make_pair(total, count[k] + total);
-	}
-
-	for (auto it = begin; it != end; ) {
-		const std::size_t elem_key = key(*it);
-		output[count[elem_key]] = std::move(*(it++));
-		count[elem_key] += 1;
-	}
-
-	// calculate output
-	if constexpr (increasing) {
-		auto it = begin;
-		for (std::size_t k = 0; k < S;) { *(it++) = std::move(output[k++]); }
-	}
-	else {
-		auto it = begin;
-		for (std::size_t k = S - 1; k > 0;) {
-			*(it++) = std::move(output[k--]);
-		}
-		*it = std::move(output[0]);
-	}
-
-	// free memory
-	delete[] output;
-	delete[] count;
+	// memory is freed automatically (see destructor)
 }
 
 } // -- namespace internal
