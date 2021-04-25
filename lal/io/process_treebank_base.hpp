24b1d3dc3de8297b4439b1eb0ac6e60d36a52721
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-04-25 12:46:21 +0200

    Renamed base class
    Added two '__' as a prefix so as to indicate users not to use this
    class.

--- lal/io/process_treebank_base.hpp
+++ lal/io/process_treebank_base.hpp
@@ -56,7 +56,7 @@ namespace io {
  * This class acts as a base class for all processor classes, namely, the
  * @ref lal::io::treebank_processor and the @ref lal::io::treebank_collection_processor.
  */
-class process_treebank_base {
+class __process_treebank_base {
 public:
 	// MODIFIERS
 
