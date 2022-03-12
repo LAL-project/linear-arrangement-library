LAL-project/linear-arrangement-library

commit bd8d9c6e2c87d6732451b43a9306ef84cbb0cfb5

Major changes in types 'linear_arrangement', 'node' and 'position'
Added class 'linear_arrangement' that stores the usual linear arrangement
and the inverse linear arrangement.

Added types 'node_t' and 'position_t' for a typesafe access to the
'linear_arrangement' class via the [] operator.

Updated all algorithms that use or create linear arrangements.

Updated CMakeLists.txt.

lal/detail/linarr/C_stack_based.hpp
@@ -55,8 +55,6 @@
 typedef std::pair<uint64_t,lal::edge> indexed_edge;
 
 #define edge_sorted_by_vertex_index(u,v) (u < v ? edge(u,v) : edge(v,u) )
-#define edge_sorted_by_pos(u,v) (pi[u] < pi[v] ? edge(u,v) : edge(v,u) )
-#define my_abs_diff(a,b) (a < b ? b - a : a - b)
 #define DECIDED_C_GT (g.get_num_edges()*g.get_num_edges() + 1)
 #define DECIDED_C_LE C
 
@@ -88,8 +86,11 @@ noexcept
 	n-1, // length of the longest edge
 	edges.size(),
 	[&](const edge& e) -> size_t {
-		const auto [u,v] = edge_sorted_by_pos(e.first, e.second);
-		++size_adjN_u[u];
+		const node_t u = e.first;
+		const node_t v = e.second;
+		++size_adjN_u[u.value];
+
+#define my_abs_diff(a,b) (a < b ? b - a : a - b)
 		return my_abs_diff(pi[u], pi[v]);
 	}
 	);
@@ -108,7 +109,9 @@ noexcept
 	// fill adjP and adjN at the same time
 	for (const auto& [uu, vv] : edges) {
 		// pi[u] < pi[v]
-		const auto [u,v] = edge_sorted_by_pos(uu, vv);
+		const auto [u,v] =
+			(pi[node_t{uu}] < pi[node_t{vv}] ? edge(uu,vv) : edge(vv,uu));
+
 		// oriented edge (u,v) "enters" node v
 		adjP[v].push_back(u);
 
@@ -133,7 +136,7 @@ template<bool decide_upper_bound>
 inline
 uint64_t __compute_C_stack_based(
 	const graphs::graph& g, const linear_arrangement& pi,
-	node * __restrict__ T, size_t * __restrict__ size_adjN_u,
+	size_t * __restrict__ size_adjN_u,
 	uint64_t upper_bound = 0
 )
 noexcept
@@ -143,11 +146,6 @@ noexcept
 	}
 
 	const uint64_t n = g.get_num_nodes();
-
-	// construct inverse arrangement
-	for (node u = 0; u < n; ++u) {
-		T[ pi[u] ] = u;
-	}
 
 	// Adjacency lists, sorted by edge length:
 	// - adjP is sorted by increasing edge length
@@ -161,8 +159,8 @@ noexcept
 	std::map<edge, uint64_t> edge_to_idx;
 
 	uint64_t idx = 0;
-	for (position pu = 0; pu < n; ++pu) {
-		const node u = T[pu];
+	for (position_t pu = 0ULL; pu < n; ++pu) {
+		const node u = pi[pu];
 		for (auto& v : adjN[u]) {
 			v.first = idx;
 
@@ -176,8 +174,8 @@ noexcept
 
 	// calculate the number of crossings
 	uint64_t C = 0;
-	for (position pu = 0; pu < n; ++pu) {
-		const node u = T[pu];
+	for (position_t pu = 0ULL; pu < n; ++pu) {
+		const node u = pi[pu];
 		for (node v : adjP[u]) {
 			const edge uv = edge_sorted_by_vertex_index(u,v);
 			const auto on_top = S.remove(indexed_edge(edge_to_idx[uv], uv));
@@ -209,15 +207,11 @@ noexcept
 	const uint64_t n = g.get_num_nodes();
 	if (n < 4) { return 0; }
 
-	// inverse function of the linear arrangement:
-	// T[p] = u <-> node u is at position p
-	data_array<node> T(n, 0);
-
 	// size_adjN_u[u] := size of adjN[u]
 	// (adjN declared and defined inside the algorithm)
 	data_array<size_t> size_adjN_u(n, 0);
 
-	return __compute_C_stack_based<false>(g, pi, T.data(), size_adjN_u.data());
+	return __compute_C_stack_based<false>(g, pi, size_adjN_u.data());
 }
 
 // ------------------
@@ -251,10 +245,6 @@ noexcept
 	std::vector<uint64_t> cs(pis.size(), 0);
 	if (n < 4) { return cs; }
 
-	// inverse function of the linear arrangement:
-	// T[p] = u <-> node u is at position p
-	data_array<node> T(n,0);
-
 	// size_adjN_u[u] := size of adjN[u]
 	// (adjN declared and defined inside the algorithm)
 	data_array<size_t> size_adjN_u(n, 0);
@@ -268,7 +258,7 @@ noexcept
 
 		// compute C
 		cs[i] = __compute_C_stack_based<false>
-				(g, pis[i], T.data(), size_adjN_u.data());
+				(g, pis[i], size_adjN_u.data());
 	}
 
 	return cs;
@@ -288,16 +278,12 @@ noexcept
 	const uint64_t n = g.get_num_nodes();
 	if (n < 4) { return 0; }
 
-	// inverse function of the linear arrangement:
-	// T[p] = u <-> node u is at position p
-	data_array<node> T(n, 0);
-
 	// size_adjN_u[u] := size of adjN[u]
 	// (adjN declared and defined inside the algorithm)
 	data_array<size_t> size_adjN_u(n, 0);
 
 	return __compute_C_stack_based<true>
-			(g, pi, T.data(), size_adjN_u.data(), upper_bound);
+			(g, pi, size_adjN_u.data(), upper_bound);
 }
 
 // ------------------
@@ -334,10 +320,6 @@ noexcept
 	std::vector<uint64_t> cs(pis.size(), 0);
 	if (n < 4) { return cs; }
 
-	// inverse function of the linear arrangement:
-	// T[p] = u <-> node u is at position p
-	data_array<node> T(n,0);
-
 	// size_adjN_u[u] := size of adjN[u]
 	// (adjN declared and defined inside the algorithm)
 	data_array<size_t> size_adjN_u(n, 0);
@@ -351,7 +333,7 @@ noexcept
 
 		// compute C
 		cs[i] = __compute_C_stack_based<true>
-				(g, pis[i], T.data(), size_adjN_u.data(), upper_bound);
+				(g, pis[i], size_adjN_u.data(), upper_bound);
 	}
 
 	return cs;
@@ -375,10 +357,6 @@ noexcept
 	std::vector<uint64_t> cs(pis.size(), 0);
 	if (n < 4) { return cs; }
 
-	// inverse function of the linear arrangement:
-	// T[p] = u <-> node u is at position p
-	data_array<node> T(n,0);
-
 	// size_adjN_u[u] := size of adjN[u]
 	// (adjN declared and defined inside the algorithm)
 	data_array<size_t> size_adjN_u(n, 0);
@@ -392,7 +370,7 @@ noexcept
 
 		// compute C
 		cs[i] = __compute_C_stack_based<true>
-				(g, pis[i], T.data(), size_adjN_u.data(), upper_bounds[i]);
+				(g, pis[i], size_adjN_u.data(), upper_bounds[i]);
 	}
 
 	return cs;

lal/CMakeLists.txt
@@ -339,15 +339,16 @@ target_sources(
 
 		# lal namespace
 		# -- heaers
+		basic_types.hpp
 		C.hpp
 		D.hpp
-		definitions.hpp
 		generate.hpp
 		graphs.hpp
 		io.hpp
 		iterators.hpp
 		lal_version.hpp
 		linarr.hpp
+		linear_arrangement.hpp
 		numeric.hpp
 		properties.hpp
 		utilities.hpp
@@ -360,15 +361,16 @@ target_sources(
 # These are not installed through the usual PUBLIC_HEADER property,
 # but via several custom variables
 set(LIBLAL_MAIN_HEADER
+	basic_types.hpp
 	C.hpp
 	D.hpp
-	definitions.hpp
 	generate.hpp
 	graphs.hpp
 	io.hpp
 	iterators.hpp
 	lal_version.hpp
 	linarr.hpp
+	linear_arrangement.hpp
 	numeric.hpp
 	properties.hpp
 	utilities.hpp

lal/basic_types.hpp
@@ -1,0 +1,285 @@
+/*********************************************************************
+ *
+ *  Linear Arrangement Library - A library that implements a collection
+ *  algorithms for linear arrangments of graphs.
+ *
+ *  Copyright (C) 2019 - 2021
+ *
+ *  This file is part of Linear Arrangement Library. To see the full code
+ *  visit the webpage:
+ *      https://github.com/lluisalemanypuig/linear-arrangement-library.git
+ *
+ *  Linear Arrangement Library is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Affero General Public License
+ *  as published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Linear Arrangement Library is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Contact:
+ *
+ *      Lluís Alemany Puig (lalemany@cs.upc.edu)
+ *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *          CQL (Complexity and Quantitative Linguistics Lab)
+ *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *          Webpage: https://cqllab.upc.edu/people/lalemany/
+ *
+ *      Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
+ *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *          CQL (Complexity and Quantitative Linguistics Lab)
+ *          Office S124, Omega building
+ *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *          Webpage: https://cqllab.upc.edu/people/rferrericancho/
+ *
+ ********************************************************************/
+
+#pragma once
+
+// C++ includes
+#include <cinttypes>
+#include <vector>
+
+namespace lal {
+
+/// Node type. See @ref LAL_concepts__node page for further details.
+typedef uint64_t node;
+/// Node's position type.
+typedef uint64_t position;
+
+/// See @ref LAL_concepts__edge page for further details.
+typedef std::pair<node, node> edge;
+/// Edge pair type.
+typedef std::pair<edge,edge> edge_pair;
+/// List of nodes.
+typedef std::vector<node> neighbourhood;
+/// See @ref LAL_concepts__head_vector page for further details.
+typedef std::vector<uint64_t> head_vector;
+
+/// Typesafe @ref node type.
+struct node_t {
+	uint64_t value;
+
+	// CONSTRUCTORS
+
+	node_t() = default;
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t(const T& _v) noexcept : value(_v) { }
+
+	// -- ASSIGNMENT OPERATORS --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t& operator= (const T& _v) noexcept { value = _v; return *this; }
+
+	// -- COMPARISONS --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator== (const T& t) const noexcept { return value == t; }
+	bool operator== (const node_t& u) const noexcept { return value == u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator== (const T& t, const node_t& u) noexcept { return u.value == t; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator!= (const T& t) const noexcept { return value != t; }
+	bool operator!= (const node_t& u) const noexcept { return value != u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator!= (const T& t, const node_t& u) noexcept { return u.value != t; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator< (const T& t) const noexcept { return value < t; }
+	bool operator< (const node_t& u) const noexcept { return value < u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator< (const T& t, const node_t& u) noexcept { return t < u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator<= (const T& t) const noexcept { return value <= t; }
+	bool operator<= (const node_t& u) const noexcept { return value <= u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator<= (const T& t, const node_t& u) noexcept { return t <= u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator> (const T& t) const noexcept { return value > t; }
+	bool operator> (const node_t& u) const noexcept { return value > u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator> (const T& t, const node_t& u) noexcept { return t > u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator>= (const T& t) const noexcept { return value >= t; }
+	bool operator>= (const node_t& u) const noexcept { return value >= u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator>= (const T& t, const node_t& u) noexcept { return t >= u.value; }
+
+	// -- ARITHMETIC --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t operator+ (const T& t) const noexcept { return node_t{value + t}; }
+	node_t operator+ (const node_t& u) const noexcept { return node_t{value + u.value}; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend node_t operator+ (const T& t, const node_t& u) noexcept { return node_t{u.value + t}; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t& operator+= (const T& t) noexcept { value += t; return *this; }
+	node_t& operator+= (const node_t& u) noexcept { value += u.value; return *this; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t operator- (const T& t) const noexcept { return node_t{value - t}; }
+	node_t operator- (const node_t& u) const noexcept { return node_t{value - u.value}; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend node_t operator- (const T& t, const node_t& u) noexcept { return node_t{t - u.value}; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	node_t& operator-= (const T& t) noexcept { value -= t; return *this; }
+	node_t& operator-= (const node_t& u) noexcept { value -= u.value; return *this; }
+
+	node_t& operator++() noexcept { ++value; return *this; }
+
+	// input/output operators
+
+	template<class stream>
+	friend stream& operator>> (stream& s, node_t& p) noexcept {
+		s >> p.value;
+		return s;
+	}
+
+	template<class stream>
+	friend stream& operator<< (stream& s, const node_t& p) noexcept {
+		s << p.value;
+		return s;
+	}
+};
+
+/// Similar to @ref edge.
+typedef std::pair<node_t, node_t> edge_t;
+/// Similar to @ref edge_pair.
+typedef std::pair<edge_t, edge_t> edge_pair_t;
+
+/// Typesafe @ref position type.
+struct position_t {
+	uint64_t value;
+
+	// CONSTRUCTORS
+
+	position_t() = default;
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t(const T& _v) noexcept : value(_v) { }
+
+	// -- ASSIGNMENT OPERATORS --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t& operator= (const T& _v) noexcept { value = _v; return *this; }
+
+	// -- COMPARISONS --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator== (const T& t) const noexcept { return value == t; }
+	bool operator== (const position_t& u) const noexcept { return value == u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator== (const T& t, const position_t& u) noexcept { return u.value == t; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator!= (const T& t) const noexcept { return value != t; }
+	bool operator!= (const position_t& u) const noexcept { return value != u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator!= (const T& t, const position_t& u) noexcept { return u.value != t; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator< (const T& t) const noexcept { return value < t; }
+	bool operator< (const position_t& u) const noexcept { return value < u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator< (const T& t, const position_t& u) noexcept { return t < u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator<= (const T& t) const noexcept { return value <= t; }
+	bool operator<= (const position_t& u) const noexcept { return value <= u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator<= (const T& t, const position_t& u) noexcept { return t <= u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator> (const T& t) const noexcept { return value > t; }
+	bool operator> (const position_t& u) const noexcept { return value > u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator> (const T& t, const position_t& u) noexcept { return t > u.value; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	bool operator>= (const T& t) const noexcept { return value >= t; }
+	bool operator>= (const position_t& u) const noexcept { return value >= u.value; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend bool operator>= (const T& t, const position_t& u) noexcept { return t >= u.value; }
+
+	// -- ARITHMETIC --
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t operator+ (const T& t) const noexcept { return position_t{value + t}; }
+	position_t operator+ (const position_t& u) const noexcept { return position_t{value + u.value}; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend position_t operator+ (const T& t, const position_t& u) noexcept { return position_t{u.value + t}; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t& operator+= (const T& t) noexcept { value += t; return *this; }
+	position_t& operator+= (const position_t& u) noexcept { value += u.value; return *this; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t operator- (const T& t) const noexcept { return position_t{value - t}; }
+	position_t operator- (const position_t& u) const noexcept { return position_t{value - u.value}; }
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	friend position_t operator- (const T& t, const position_t& u) noexcept { return position_t{t - u.value}; }
+
+	template<typename T,std::enable_if_t<std::is_integral_v<T>, bool> = true>
+	position_t& operator-= (const T& t) noexcept { value -= t; return *this; }
+	position_t& operator-= (const position_t& u) noexcept { value -= u.value; return *this; }
+
+	position_t& operator++() noexcept { ++value; return *this; }
+
+	// input/output operators
+
+	template<class stream>
+	friend stream& operator>> (stream& s, position_t& p) noexcept {
+		s >> p.value;
+		return s;
+	}
+
+	template<class stream>
+	friend stream& operator<< (stream& s, const position_t& p) noexcept {
+		s << p.value;
+		return s;
+	}
+};
+
+static_assert(    std::is_trivial_v<node_t>);
+static_assert(    std::is_trivially_constructible_v<node_t>);
+static_assert(    std::is_trivially_copy_assignable_v<node_t>);
+static_assert(    std::is_trivially_copy_constructible_v<node_t>);
+static_assert(    std::is_trivially_copyable_v<node_t>);
+static_assert(    std::is_trivially_move_constructible_v<node_t>);
+static_assert(    std::is_trivially_move_assignable_v<node_t>);
+static_assert(    std::is_constructible_v<node_t, node_t>);
+static_assert(    std::is_constructible_v<node_t, node>);
+static_assert(    std::is_constructible_v<node_t, int>);
+static_assert(    std::is_trivially_assignable_v<node_t, node_t>);
+static_assert(not std::is_trivially_assignable_v<node_t, node>);
+static_assert(not std::is_trivially_assignable_v<node_t, int>);
+static_assert(    std::is_trivially_destructible_v<node_t>);
+
+static_assert(    std::is_trivial_v<position_t>);
+static_assert(    std::is_trivially_constructible_v<position_t>);
+static_assert(    std::is_trivially_copy_assignable_v<position_t>);
+static_assert(    std::is_trivially_copy_constructible_v<position_t>);
+static_assert(    std::is_trivially_copyable_v<position_t>);
+static_assert(    std::is_trivially_move_constructible_v<position_t>);
+static_assert(    std::is_trivially_move_assignable_v<position_t>);
+static_assert(    std::is_constructible_v<position_t, position_t>);
+static_assert(    std::is_constructible_v<position_t, position>);
+static_assert(    std::is_constructible_v<position_t, int>);
+static_assert(    std::is_trivially_assignable_v<position_t, position_t>);
+static_assert(not std::is_trivially_assignable_v<position_t, position>);
+static_assert(not std::is_trivially_assignable_v<position_t, int>);
+static_assert(    std::is_trivially_destructible_v<position_t>);
+
+} // -- namespace lal

lal/definitions.hpp
@@ -1,67 +1,0 @@
-/*********************************************************************
- *
- *  Linear Arrangement Library - A library that implements a collection
- *  algorithms for linear arrangments of graphs.
- *
- *  Copyright (C) 2019 - 2021
- *
- *  This file is part of Linear Arrangement Library. To see the full code
- *  visit the webpage:
- *      https://github.com/lluisalemanypuig/linear-arrangement-library.git
- *
- *  Linear Arrangement Library is free software: you can redistribute it
- *  and/or modify it under the terms of the GNU Affero General Public License
- *  as published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- *
- *  Linear Arrangement Library is distributed in the hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Contact:
- *
- *      Lluís Alemany Puig (lalemany@cs.upc.edu)
- *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
- *          CQL (Complexity and Quantitative Linguistics Lab)
- *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
- *          Webpage: https://cqllab.upc.edu/people/lalemany/
- *
- *      Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
- *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
- *          CQL (Complexity and Quantitative Linguistics Lab)
- *          Office S124, Omega building
- *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
- *          Webpage: https://cqllab.upc.edu/people/rferrericancho/
- *
- ********************************************************************/
-
-#pragma once
-
-// C++ includes
-#include <cstdint>
-#include <vector>
-
-namespace lal {
-
-/// Node type. See @ref LAL_concepts__node page for further details.
-typedef uint64_t node;
-/// Node's position type.
-typedef uint64_t position;
-
-/// See @ref LAL_concepts__linear_arrangement page for further details.
-typedef std::vector<position> linear_arrangement;
-
-/// See @ref LAL_concepts__edge page for further details.
-typedef std::pair<node, node> edge;
-/// Edge pair type.
-typedef std::pair<edge,edge> edge_pair;
-/// List of nodes.
-typedef std::vector<node> neighbourhood;
-/// See @ref LAL_concepts__head_vector page for further details.
-typedef std::vector<uint64_t> head_vector;
-
-} // -- namespace lal

lal/detail/data_array.hpp
@@ -69,7 +69,6 @@ public:
 	data_array(const std::size_t n, const T& v) noexcept : data_array(n) {
 		fill(v);
 	}
-
 	// Copy constructor
 	data_array(const data_array& d) noexcept : data_array(d.m_size) {
 		if (m_size > 0) {
@@ -209,7 +208,7 @@ private:
 		m_data = m_size == 0 ? nullptr : new T[m_size];
 	}
 
-private:
+protected:
 	// the data of this array
 	T *m_data = nullptr;
 	// the size of this array in number of elements

lal/detail/graphs/conversions.hpp
@@ -46,7 +46,7 @@
 #include <optional>
 
 // lal includes
-#include <lal/definitions.hpp>
+#include <lal/basic_types.hpp>
 #include <lal/graphs/free_tree.hpp>
 #include <lal/graphs/rooted_tree.hpp>
 #include <lal/detail/data_array.hpp>

lal/detail/graphs/make_arrangement.hpp
@@ -54,14 +54,11 @@ namespace detail {
 
 namespace __lal {
 
-template<
-	template<typename... Args> class oContainer,
-	template<typename... Args> class iContainer, typename... iTypes
->
+template<class container>
 inline
 void __make_arrangement_intervals(
 	const graphs::rooted_tree& T, node r,
-	const oContainer<iContainer<iTypes...>>& data,
+	const container& data,
 	position& pos, linear_arrangement& arr
 )
 noexcept
@@ -71,14 +68,14 @@ noexcept
 
 	// vertex 'r' is a leaf
 	if (d_out == 0) {
-		arr[r] = pos++;
+		arr.assign(r, pos++);
 		return;
 	}
 	const auto& interval = data[r];
 	for (size_t i = 0; i < interval.size(); ++i) {
 		const node vi = interval[i];
 		if (vi == r) {
-			arr[vi] = pos++;
+			arr.assign(vi, pos++);
 		}
 		else {
 			__make_arrangement_intervals(T, vi, data, pos, arr);
@@ -88,14 +85,11 @@ noexcept
 
 } // namespace __lal
 
-template<
-	template<typename... Args> class oContainer,
-	template<typename... Args> class iContainer, typename... iTypes
->
+template<class container>
 inline
 linear_arrangement make_arrangement_intervals(
 	const graphs::rooted_tree& T,
-	const oContainer<iContainer<iTypes...>>& data
+	const container& data
 )
 noexcept
 {
@@ -110,14 +104,11 @@ noexcept
 
 namespace __lal {
 
-template<
-	template<typename... Args> class oContainer,
-	template<typename... Args> class iContainer, typename... iTypes
->
+template<class container>
 inline
 void __make_arrangement_intervals(
 	const graphs::free_tree& T, node parent, node u,
-	const oContainer<iContainer<iTypes...>>& data,
+	const container& data,
 	position& pos, linear_arrangement& arr
 )
 noexcept
@@ -127,14 +118,14 @@ noexcept
 
 	// vertex 'u' is a leaf in the rooted version of T
 	if (d_out == 0) {
-		arr[u] = pos++;
+		arr.assign(u, pos++);
 		return;
 	}
 	const auto& interval = data[u];
 	for (size_t i = 0; i < interval.size(); ++i) {
 		const node vi = interval[i];
 		if (vi == u) {
-			arr[vi] = pos++;
+			arr.assign(vi, pos++);
 		}
 		else {
 			__make_arrangement_intervals(T, u, vi, data, pos, arr);
@@ -143,14 +134,11 @@ noexcept
 }
 } // -- namespace __lal
 
-template<
-	template<typename... Args> class oContainer,
-	template<typename... Args> class iContainer, typename... iTypes
->
+template<class container>
 inline
 linear_arrangement make_arrangement_intervals(
 	const graphs::free_tree& T, node root,
-	const oContainer<iContainer<iTypes...>>& data
+	const container& data
 )
 noexcept
 {

lal/detail/graphs/traversal.hpp
@@ -46,7 +46,7 @@
 #include <queue>
 
 // lal includes
-#include <lal/definitions.hpp>
+#include <lal/basic_types.hpp>
 #include <lal/graphs/directed_graph.hpp>
 #include <lal/graphs/undirected_graph.hpp>
 #include <lal/detail/data_array.hpp>

commit a05e52015ec7518db6a1dceb0ad01200ede7a9fb

Fixed bugs
Fixed a bug that was introduced in commit
	bd8d9c6e2c87d6732451b43a9306ef84cbb0cfb5
with date 27 Oct 2021... Sorry.

lal/detail/linarr/C_stack_based.hpp
@@ -43,6 +43,7 @@
 #if defined DEBUG
 #include <cassert>
 #endif
+#include <iostream>
 #include <map>
 
 // lal includes
@@ -70,7 +71,7 @@ namespace __lal {
 
 template<class graph_t>
 void fill_adjP_adjN(
-	const graph_t& g, const linear_arrangement& pi,
+	const graph_t& g, const linear_arrangement& arr,
 	std::vector<neighbourhood>& adjP,
 	std::vector<std::vector<indexed_edge>>& adjN,
 	std::size_t * const size_adjN_u
@@ -87,13 +88,17 @@ noexcept
 		<edge, std::vector<edge>::iterator, countingsort::non_decreasing_t>
 		(
 		edges.begin(), edges.end(),
-		n-1, // length of the longest edge
+		n - 1, // length of the longest edge
 		edges.size(),
 		[&](const edge& e) -> std::size_t {
-			const node_t u = e.first;
-			const node_t v = e.second;
-			++size_adjN_u[*u];
-			return abs_diff(pi[u], pi[v]);
+
+			const node uu = e.first;
+			const node vv = e.second;
+			const auto [u,v] =
+				(arr[node_t{uu}] < arr[node_t{vv}] ? edge(uu,vv) : edge(vv,uu));
+
+			++size_adjN_u[u];
+			return abs_diff(arr[node_t{u}], arr[node_t{v}]);
 		}
 		);
 
@@ -102,8 +107,12 @@ noexcept
 		// divide by two because the 'key' function in the call to
 		// the sorting function is called twice for every edge
 #if defined DEBUG
-		assert( (size_adjN_u[u]%2) == 0 );
+		assert((size_adjN_u[u]%2) == 0);
+		// the assertion
+		//     assert(size_adjN_u[u] != 0);
+		// is wrong.
 #endif
+
 		size_adjN_u[u] /= 2;
 		adjN[u].resize(size_adjN_u[u]);
 	}
@@ -112,7 +121,7 @@ noexcept
 	for (const auto& [uu, vv] : edges) {
 		// pi[u] < pi[v]
 		const auto [u,v] =
-			(pi[node_t{uu}] < pi[node_t{vv}] ? edge(uu,vv) : edge(vv,uu));
+			(arr[node_t{uu}] < arr[node_t{vv}] ? edge(uu,vv) : edge(vv,uu));
 
 		// oriented edge (u,v) "enters" node v
 		adjP[v].push_back(u);
@@ -121,6 +130,7 @@ noexcept
 		--size_adjN_u[u];
 		adjN[u][size_adjN_u[u]] = indexed_edge(0, edge_sorted_by_vertex_index(u,v));
 	}
+
 #if defined DEBUG
 	for (node u = 0; u < n; ++u) {
 		assert(size_adjN_u[u] == 0);