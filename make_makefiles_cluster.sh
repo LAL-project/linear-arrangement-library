13a3ce1cbf36706d2d1b40cd22d1fd5877509e44
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2022-11-30 11:24:45 +0100

    Make makefiles with 'LTO=YES'

--- make_makefiles_cluster.sh
+++ make_makefiles_cluster.sh
@@ -2,10 +2,10 @@
 
 # ----------------------------------------------------------------------
 mkdir -p lal-release && cd lal-release
-qmake -makefile -o Makefile "ENVIR=CLUSTER" ../lal/lal.pro
+qmake -makefile -o Makefile "LTO=YES" "ENVIR=CLUSTER" ../lal/lal.pro
 cd ..
 
 # ----------------------------------------------------------------------
 mkdir -p lal-debug && cd lal-debug
-qmake -makefile -o Makefile "ENVIR=CLUSTER" CONFIG+=debug ../lal/lal.pro
+qmake -makefile -o Makefile "LTO=YES" "ENVIR=CLUSTER" CONFIG+=debug ../lal/lal.pro
 cd ..
