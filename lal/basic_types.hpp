23ba014ecea059578bc475036c10f99c253e7894
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-01-11 12:06:06 +0100

    Updated license header

--- lal/basic_types.hpp
+++ lal/basic_types.hpp
@@ -1,41 +1,43 @@
+
+
 /*********************************************************************
  *
- *  Linear Arrangement Library - A library that implements a collection
- *  algorithms for linear arrangments of graphs.
+ * Linear Arrangement Library - A library that implements a collection
+ * algorithms for linear arrangments of graphs.
  *
- *  Copyright (C) 2019 - 2022
+ * Copyright (C) 2019 - 2023
  *
- *  This file is part of Linear Arrangement Library. The full code is available
- *  at:
+ * This file is part of Linear Arrangement Library. The full code is available
+ * at:
  *      https://github.com/LAL-project/linear-arrangement-library.git
  *
- *  Linear Arrangement Library is free software: you can redistribute it
- *  and/or modify it under the terms of the GNU Affero General Public License
- *  as published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
+ * Linear Arrangement Library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  Linear Arrangement Library is distributed in the hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU Affero General Public License for more details.
+ * Linear Arrangement Library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Contact:
+ * Contact:
  *
- *      Lluís Alemany Puig (lalemany@cs.upc.edu)
- *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
- *          CQL (Complexity and Quantitative Linguistics Lab)
- *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
- *          Webpage: https://cqllab.upc.edu/people/lalemany/
+ *     Lluís Alemany Puig (lalemany@cs.upc.edu)
+ *         LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *         CQL (Complexity and Quantitative Linguistics Lab)
+ *         Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *         Webpage: https://cqllab.upc.edu/people/lalemany/
  *
- *      Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
- *          LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
- *          CQL (Complexity and Quantitative Linguistics Lab)
- *          Office S124, Omega building
- *          Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
- *          Webpage: https://cqllab.upc.edu/people/rferrericancho/
+ *     Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
+ *         LARCA (Laboratory for Relational Algorithmics, Complexity and Learning)
+ *         CQL (Complexity and Quantitative Linguistics Lab)
+ *         Office S124, Omega building
+ *         Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *         Webpage: https://cqllab.upc.edu/people/rferrericancho/
  *
  ********************************************************************/
 
