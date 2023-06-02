ac940d1c2cfd98092a137480f2ad86c476ab6eb5
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-06-02 17:00:35 +0200

    Updated scripts for compilation in the cluster

--- make_makefiles_cluster.sh
+++ make_makefiles_cluster.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-# ----------------------------------------------------------------------
-mkdir -p lal-release && cd lal-release
-qmake -makefile -o Makefile "ENVIR=CLUSTER" ../lal/lal.pro
-cd ..
+CMAKE_DIR=/home/soft/cmake-3.22.1/bin
 
-# ----------------------------------------------------------------------
-mkdir -p lal-debug && cd lal-debug
-qmake -makefile -o Makefile "ENVIR=CLUSTER" CONFIG+=debug ../lal/lal.pro
-cd ..
+function make_directories_compiler() {
+	local compiler=$1
+
+	local suffix=""
+	local CXX_compiler=""
+	local C_compiler=""
+	if [ ! -z $compiler ]; then
+		suffix="-$compiler"
+		CXX_compiler="-DCMAKE_CXX_COMPILER=/home/soft/gcc-$compiler/bin/g++"
+		C_compiler="-DCMAKE_C_COMPILER=/home/soft/gcc-$compiler/bin/gcc"
+	else
+		CXX_compiler="-DCMAKE_CXX_COMPILER=/home/soft/gcc-11.3/bin/g++"
+		C_compiler="-DCMAKE_C_COMPILER=/home/soft/gcc-11.3/bin/gcc"
+	fi
+	
+	mkdir -p lal-debug$suffix
+	cd lal-debug$suffix
+	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Debug ../lal
+	cd ..
+
+	mkdir -p lal-release$suffix
+	cd lal-release$suffix
+	$CMAKE_DIR/cmake $CXX_compiler $C_compiler -DCMAKE_BUILD_TYPE=Release ../lal
+	cd ..
+}
+
+make_directories_compiler
+make_directories_compiler 11.3
+make_directories_compiler 12.3
+make_directories_compiler 13.1
