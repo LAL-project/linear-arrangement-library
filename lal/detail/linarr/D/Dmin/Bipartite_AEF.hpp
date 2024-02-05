986d600086140d792bbdb9d843fc21fd9f8c3570
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-02-05 09:55:26 +0100

    Reuse code for Bipartite MaxLA and Bipartite minLA

--- lal/detail/linarr/D/Dmin/Bipartite_AEF.hpp
+++ lal/detail/linarr/D/Dmin/Bipartite_AEF.hpp
@@ -50,6 +50,7 @@
 #include <lal/graphs/undirected_graph.hpp>
 #include <lal/detail/properties/bipartite_graph_colorability.hpp>
 #include <lal/detail/sorting/counting_sort.hpp>
+#include <lal/detail/linarr/D/Dopt_utils.hpp>
 
 namespace lal {
 namespace detail {
@@ -64,8 +65,8 @@ namespace bipartite {
  * arrangement should be returned.
  * @tparam graph_t Type of graph. Any subclass of @ref lal::graphs::graph.
  * @param g Input (bipartite) graph.
- * @param c Coloring of the input graph.
- * @returns The cost of a maximal bipartite arrangement and possibly the arrangement
+ * @param c Bipartite coloring of the input graph.
+ * @returns The cost of a minimum bipartite arrangement and possibly the arrangement
  * that attains it.
  * @pre The input graph is a bipartite graph.
  */
@@ -74,98 +75,13 @@ std::conditional_t<
 	make_arrangement,
 	std::pair<uint64_t, linear_arrangement>,
 	uint64_t
->
+	>
 AEF(const graph_t& g, const properties::bipartite_graph_coloring& c) noexcept {
 	static_assert(std::is_base_of_v<graphs::graph, graph_t>);
-
-	const auto n = g.get_num_nodes();
-
-	// annoying corner case
-	if (n == 1) {
-		if constexpr (make_arrangement) {
-			return {0, lal::linear_arrangement::identity(n)};
-		}
-		else {
-			return 0;
-		}
-	}
-
-	data_array<node> vertices_color_1(n - 1);
-	std::size_t size_1 = 0;
-	data_array<node> vertices_color_2(n - 1);
-	std::size_t size_2 = 0;
-
-	{
-	const auto first_color = c[0];
-	for (lal::node u = 0; u < n; ++u) {
-		if (c[u] == first_color) {
-			vertices_color_1[size_1++] = u;
-		}
-		else {
-			vertices_color_2[size_2++] = u;
-		}
-	}
-
-	const auto sort_nodes =
-	[&](data_array<node>& nodes, std::size_t s) {
-		lal::detail::sorting::counting_sort
-			<lal::node, lal::detail::sorting::non_increasing_t>
-			(
-				nodes.begin(), nodes.begin() + s,
-				n - 1,
-				s,
-				[&](const lal::node u) -> std::size_t {
-					// in directed graphs, this function returns the sum of the
-					// in-degree plus the out-degree of the vertex.
-					return g.get_degree(u);
-				}
-			);
-	};
-	sort_nodes(vertices_color_1, size_1);
-	sort_nodes(vertices_color_2, size_2);
-	}
-
-	uint64_t D = 0;
-	lal::linear_arrangement arr;
-
-	if constexpr (make_arrangement) {
-		arr.resize(n);
-	}
-
-	lal::position p = 0;
-	for (std::size_t i = size_1 - 1; i > 0; --i) {
-		const node u = vertices_color_1[i];
-		if constexpr (make_arrangement) {
-			arr.assign(u, p);
-			++p;
-		}
-		D += (n - p)*g.get_degree(u);
-	}
-
-	{
-	const node u = vertices_color_1[0];
-	if constexpr (make_arrangement) {
-		arr.assign(vertices_color_1[0], p);
-		++p;
-	}
-	D += (n - p)*g.get_degree(u);
-	}
-
-	for (std::size_t i = 0; i < size_2; ++i) {
-		const node u = vertices_color_2[i];
-		if constexpr (make_arrangement) {
-			arr.assign(u, p);
-			++p;
-		}
-		D -= (n - p)*g.get_degree(u);
-	}
-
-	if constexpr (make_arrangement) {
-		return {D, std::move(arr)};
-	}
-	else {
-		return D;
-	}
+	return
+		Dopt_utils::optimal_bipartite_arrangement_AEF
+		<make_arrangement, lal::detail::sorting::non_increasing_t>
+		(g, c);
 }
 
 /**
@@ -176,7 +92,7 @@ AEF(const graph_t& g, const properties::bipartite_graph_coloring& c) noexcept {
  * arrangement should be returned.
  * @tparam graph_t Type of graph. Any subclass of @ref lal::graphs::graph.
  * @param g Input (bipartite) graph.
- * @returns The cost of a maximal bipartite arrangement and possibly the arrangement
+ * @returns The cost of a minimum bipartite arrangement and possibly the arrangement
  * that attains it.
  * @pre The input graph is a bipartite graph.
  */
@@ -189,7 +105,7 @@ std::conditional_t<
 AEF(const graph_t& g) noexcept {
 	static_assert(std::is_base_of_v<graphs::graph, graph_t>);
 	const auto c = color_vertices_graph(g);
-	return AEF<make_arrangement>(g, c);
+	return AEF(g, c);
 }
 
 } // -- namespace bipartite
