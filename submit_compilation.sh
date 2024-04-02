9263e924bff71aaf89853d0f7e074da5b106a0b4
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-04-02 10:46:27 +0200

    Messages to identify which job is running what

--- submit_compilation.sh
+++ submit_compilation.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 ./make_makefiles_cluster.sh
+echo "release: g++ system-wide "
 sbatch -p short -c 8 compile_cluster.sh lal-release
+echo "debug: g++ system-wide"
 sbatch -p short -c 8 compile_cluster.sh lal-debug
+
+echo "release: g++ 11.3"
 sbatch -p short -c 8 compile_cluster.sh lal-release-11.3
+echo "debug: g++ 11.3"
 sbatch -p short -c 8 compile_cluster.sh lal-debug-11.3
+
+echo "release: g++ 11.4.0"
 sbatch -p short -c 8 compile_cluster.sh lal-release-11.4.0
+echo "debug: g++ 11.4.0"
 sbatch -p short -c 8 compile_cluster.sh lal-debug-11.4.0
+
+echo "release: g++ 12.3"
 sbatch -p short -c 8 compile_cluster.sh lal-release-12.3
+echo "debug: g++ 12.3"
 sbatch -p short -c 8 compile_cluster.sh lal-debug-12.3
+
+echo "release: g++ 13.1"
 sbatch -p short -c 8 compile_cluster.sh lal-release-13.1
+echo "debug: g++ 13.1"
 sbatch -p short -c 8 compile_cluster.sh lal-debug-13.1
