3c2dd1027f96b08ac4a85d1943666dd707c5168f
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-07-10 15:12:58 +0200

    Use correct license header and acknowledge everyone

--- lal/detail/linarr/D/DMax/unconstrained/branch_and_bound/AEF/BnB_roll_back__antenna.cpp
+++ lal/detail/linarr/D/DMax/unconstrained/branch_and_bound/AEF/BnB_roll_back__antenna.cpp
@@ -1,29 +1,49 @@
-/***********************************************************************
+/*********************************************************************
  *
- * Research on Linear Arrangements project
- * Copyright (C) 2019 - 2024 Lluís Alemany Puig
+ * Linear Arrangement Library - A library that implements a collection
+ * algorithms for linear arrangments of graphs.
  *
- * The full code is available at:
- *     https://github.com/lluisalemanypuig/research-linear-arrangements.git
+ * Copyright (C) 2019 - 2024
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is part of Linear Arrangement Library. The full code is available
+ * at:
+ *      https://github.com/LAL-project/linear-arrangement-library.git
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Linear Arrangement Library is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Linear Arrangement Library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Linear Arrangement Library.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Contact:
  *
- *    Lluís Alemany Puig (lluis.alemany.puig@upc.edu)
+ *     Lluís Alemany Puig (lluis.alemany.puig@upc.edu)
+ *         LQMC (Quantitative, Mathematical, and Computational Linguisitcs)
+ *         CQL (Complexity and Quantitative Linguistics Lab)
+ *         Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *         Webpage: https://cqllab.upc.edu/people/lalemany/
  *
- ***********************************************************************/
+ *     Juan Luis Esteban (esteban@cs.upc.edu)
+ *         LOGPROG: Logics and Programming Research Group
+ *         Office 110, Omega building
+ *         Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *         Webpage: https://www.cs.upc.edu/~esteban/
+ *
+ *     Ramon Ferrer i Cancho (rferrericancho@cs.upc.edu)
+ *         LQMC (Quantitative, Mathematical, and Computational Linguisitcs)
+ *         CQL (Complexity and Quantitative Linguistics Lab)
+ *         Office 220, Omega building
+ *         Jordi Girona St 1-3, Campus Nord UPC, 08034 Barcelona.   CATALONIA, SPAIN
+ *         Webpage: https://cqllab.upc.edu/people/rferrericancho/
+ *
+ ********************************************************************/
 
 // C++ includes
 #if defined DEBUG
