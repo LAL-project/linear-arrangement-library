b95bdd5f61ee66a4559dce9d48959fef0c02ca40
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2026-01-07 09:05:00 +0100

    Updated copyright years

--- lal/generate/rand_arrangements.cpp
+++ lal/generate/rand_arrangements.cpp
@@ -3,7 +3,7 @@
  * Linear Arrangement Library - A library that implements a collection
  * algorithms for linear arrangments of graphs.
  *
- * Copyright (C) 2019 - 2025
+ * Copyright (C) 2019 - 2026
  *
  * This file is part of Linear Arrangement Library. The full code is available
  * at:
